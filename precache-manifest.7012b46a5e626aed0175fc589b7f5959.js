df8e9193dcde2c165526e9c6b42ebb50f67b3dcc
arvindthetech <patelkumararvind1234@gmail.com>
2025-02-15 20:02:25 +0530

    Updates

---
+++ precache-manifest.7012b46a5e626aed0175fc589b7f5959.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b06925dcf25ab0d18420739699852e0",
+    "url": "/pw/index.html"
+  },
+  {
+    "revision": "fa0ecb1551f87c196f59",
+    "url": "/pw/static/css/2.4e461ce0.chunk.css"
+  },
+  {
+    "revision": "f54e4f406a8d951322c1",
+    "url": "/pw/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fa0ecb1551f87c196f59",
+    "url": "/pw/static/js/2.bd03f4bf.chunk.js"
+  },
+  {
+    "revision": "f54e4f406a8d951322c1",
+    "url": "/pw/static/js/main.878e99d3.chunk.js"
+  },
+  {
+    "revision": "88312f151f5a37522869",
+    "url": "/pw/static/js/runtime~main.c1c3737a.js"
+  }
+]);
